d6f4398985c575dea8b4f5694e3e81f4839cb826
Dogguy0210 <39935185+Dogguy0210@users.noreply.github.com>
2020-04-12 13:42:58 -0700

    Revert "A cool little test feature"

--- Database.cpp
+++ Database.cpp
@@ -38,41 +38,6 @@ bool Database::UpdateProduct(QString name, QString price) { return false; }
 // Remove Product
 bool Database::DeleteProduct(QString productID) { return false; }
 
-QString Database::findDatabase(QString fileName)
-{
-    QDir    databaseSearch; //The QDir object used to file the database file
-    QString databasePath;   //The path of the database file, equals NOT FOUND
-                            //if database file could not be found
-
-    databaseSearch = QDir::currentPath();
-
-    databaseSearch.cdUp();
-
-    bool projectDirectoryFound = databaseSearch.cd("Bulk-Club-Project");
-
-    if (projectDirectoryFound)
-    {
-        if (databaseSearch.exists(fileName))
-        {
-            databasePath = databaseSearch.filePath(fileName);
-        }
-        else
-        {
-            databasePath = "NOT FOUND";
-
-            qDebug() << "Error: The database file could not be found.";
-        }
-    }
-    else
-    {
-        databasePath = "NOT FOUND";
-
-        qDebug() << "Error: The project directory could not be found.";
-    }
-
-    return databasePath;
-}
-
 // PUT ALL YALL'S QUERIES DOWN HERE. IF IT'S PURELY A TABLEVIEW QUERY TO
 // DISPLAY DATA THEN YOU DONT NEED TO RUN QUERIES HERE
 
