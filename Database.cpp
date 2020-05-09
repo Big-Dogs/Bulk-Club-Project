a62755e20418d5e6cd805753852f2526feee5ceb
Weston Mathews <westonmathews@gmail.com>
2020-05-09 11:12:56 -0700

    Removed empty methods.

--- Database.cpp
+++ Database.cpp
@@ -266,31 +266,6 @@ QVector<Database::Member> Database::GetExecutiveMemberPurchases(QStringList exec
    return executivePurchaseAr;
 }
 
-
-
-// Add Member
-bool Database::AddCustomer(QString name, QString isExecutive, QString expireDate) { return false; }
-
-// Edit Member Data
-bool Database::UpdateCustomer(QString name, QString isExecutive, QString expireDate) { return false; }
-
-// Remove Member
-bool Database::DeleteCustomer(QString memberID) { return false; }
-
-// Add Product
-bool Database::AddProduct(QString name, QString price) { return false; }
-
-// Edit Product Data
-bool Database::UpdateProduct(QString name, QString price) { return false; }
-
-// Remove Product
-bool Database::DeleteProduct(QString productID) { return false; }
-
-
-
-// PUT ALL YALL'S QUERIES DOWN HERE. IF IT'S PURELY A TABLEVIEW QUERY TO
-// DISPLAY DATA THEN YOU DONT NEED TO RUN QUERIES HERE
-
 double Database::getPrice(QString item)
 {
     double purchaseAmt = 0;
